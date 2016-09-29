ed6d2542b61bee24a382286385c991ed0797b5ca
Mohd Amiruddin Zainol <mohd.amiruddin.zainol@gmail.com>
2016-09-29 16:38:14 +0100

    Update helloworld.c

--- vivado-2014.4/two-dma-inputs-window-kernel/SDK/helloworld.c
+++ vivado-2014.4/two-dma-inputs-window-kernel/SDK/helloworld.c
@@ -6,6 +6,7 @@
 
 #define DMA_DEV_ID0		XPAR_AXI_DMA_0_DEVICE_ID
 #define DMA_DEV_ID1		XPAR_AXI_DMA_1_DEVICE_ID
+#define DMA_DEV_ID2		XPAR_AXI_DMA_2_DEVICE_ID // Loopback DMA
 #define DMA_DEV_ID3		XPAR_AXI_DMA_3_DEVICE_ID
 
 #define MEM_BASE_ADDR		(XPAR_PS7_DDR_0_S_AXI_BASEADDR + 0x10100000)
@@ -13,8 +14,9 @@
 #define TX_BUFFER_BASE0		(MEM_BASE_ADDR + 0x00100000)
 #define TX_BUFFER_BASE1		(MEM_BASE_ADDR + 0x02100000)
 #define RX_BUFFER_BASE		(MEM_BASE_ADDR + 0x04100000) // max 8 MB axi dma
+#define TMP_BUFFER			(MEM_BASE_ADDR + 0x06100000) // Loopback DMA
 
-#define MAX_PKT_LEN_WORDS	1024
+#define MAX_PKT_LEN_WORDS	1048576
 #define MAX_PKT_LEN		MAX_PKT_LEN_WORDS*4
 
 /************************** Function Prototypes ******************************/
@@ -29,6 +31,7 @@ int init_dma(u16 DeviceId, XAxiDma * DMAptr);
  */
 XAxiDma AxiDma0;
 XAxiDma AxiDma1;
+XAxiDma AxiDma2; // Loopback DMA
 XAxiDma AxiDma3;
 
 // the HW Acc instance
@@ -55,7 +58,7 @@ int main()
 		return XST_FAILURE;
 	}
 
-	//xil_printf("XAxiDma_SimplePollExample: Passed\r\n");
+	xil_printf("XAxiDma_SimplePollExample: Passed!\r\n");
 
 	return XST_SUCCESS;
 }
@@ -67,11 +70,15 @@ int XAxiDma_SimplePollExample()
 	u32 *TxBufferPtr0;
 	u32 *TxBufferPtr1;
 	u32 *RxBufferPtr;
+	u32 *TmpBufferPtr; // Loopback DMA
 	float Value;
+	int i;
+	int sim_time = 100;
 
 	TxBufferPtr0 = (u32 *)TX_BUFFER_BASE0;
 	TxBufferPtr1 = (u32 *)TX_BUFFER_BASE1;
-	RxBufferPtr = (u32 *)RX_BUFFER_BASE;
+	RxBufferPtr  = (u32 *)RX_BUFFER_BASE;
+	TmpBufferPtr = (u32 *)TMP_BUFFER; // Loopback DMA
 
 	// Run the HW Accelerator
 	Status = XFunc_hls_core_CfgInitialize(&XAxilite_add_HW_dev,
@@ -88,6 +95,10 @@ int XAxiDma_SimplePollExample()
 	// Start the accelerator
 	XFunc_hls_core_Start(&XAxilite_add_HW_dev);
 
+	// Enable auto restart the accelerator,
+	// in such a way that it is ready for the next transaction
+	XFunc_hls_core_EnableAutoRestart(&XAxilite_add_HW_dev);
+
 	/* Initialize the XAxiDma device.
 	 */
 	Status = init_dma(DMA_DEV_ID0, &AxiDma0);
@@ -100,21 +111,21 @@ int XAxiDma_SimplePollExample()
 		return XST_FAILURE;
 	}
 
+	// Loopback DMA
+	Status = init_dma(DMA_DEV_ID2, &AxiDma2);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
 	Status = init_dma(DMA_DEV_ID3, &AxiDma3);
 	if (Status != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 
-	/*
-	Value = 1;
-	for(Index = 0; Index < MAX_PKT_LEN_WORDS; Index ++) {
-			x.f = Value+0.0f;
-			TxBufferPtr0[Index] = x.u;
-			Value++;
-	}
-	*/
-	Xil_DCacheFlushRange((u32)TxBufferPtr0, MAX_PKT_LEN);
-	Xil_DCacheFlushRange((u32)TxBufferPtr1, MAX_PKT_LEN);
+    for (i = sim_time; i !=0; i--)
+    {
+    	Xil_DCacheFlushRange((u32)TxBufferPtr0, MAX_PKT_LEN);
+    	Xil_DCacheFlushRange((u32)TxBufferPtr1, MAX_PKT_LEN);
 
 		Status = XAxiDma_SimpleTransfer(&AxiDma3,(u32) RxBufferPtr,
 					MAX_PKT_LEN, XAXIDMA_DEVICE_TO_DMA);
@@ -143,6 +154,74 @@ int XAxiDma_SimplePollExample()
 
 		while (XAxiDma_Busy(&AxiDma3,XAXIDMA_DEVICE_TO_DMA)) {}
 
+		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		// tmp = temp
+		Xil_DCacheFlushRange((u32)TxBufferPtr0, MAX_PKT_LEN);
+
+		Status = XAxiDma_SimpleTransfer(&AxiDma2,(u32) TmpBufferPtr,
+				MAX_PKT_LEN, XAXIDMA_DEVICE_TO_DMA);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		Status = XAxiDma_SimpleTransfer(&AxiDma2,(u32) TxBufferPtr0,
+				MAX_PKT_LEN, XAXIDMA_DMA_TO_DEVICE);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		while (XAxiDma_Busy(&AxiDma2,XAXIDMA_DMA_TO_DEVICE)) {}
+
+		while (XAxiDma_Busy(&AxiDma2,XAXIDMA_DEVICE_TO_DMA)) {}
+
+		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		// temp = out
+		Xil_DCacheFlushRange((u32)RxBufferPtr, MAX_PKT_LEN);
+
+		Status = XAxiDma_SimpleTransfer(&AxiDma2,(u32) TxBufferPtr0,
+			MAX_PKT_LEN, XAXIDMA_DEVICE_TO_DMA);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		Status = XAxiDma_SimpleTransfer(&AxiDma2,(u32) RxBufferPtr,
+			MAX_PKT_LEN, XAXIDMA_DMA_TO_DEVICE);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		while (XAxiDma_Busy(&AxiDma2,XAXIDMA_DMA_TO_DEVICE)) {}
+
+		while (XAxiDma_Busy(&AxiDma2,XAXIDMA_DEVICE_TO_DMA)) {}
+
+		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		// out = tmp
+		Xil_DCacheFlushRange((u32)TmpBufferPtr, MAX_PKT_LEN);
+
+		Status = XAxiDma_SimpleTransfer(&AxiDma2,(u32) RxBufferPtr,
+			MAX_PKT_LEN, XAXIDMA_DEVICE_TO_DMA);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		Status = XAxiDma_SimpleTransfer(&AxiDma2,(u32) TmpBufferPtr,
+			MAX_PKT_LEN, XAXIDMA_DMA_TO_DEVICE);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		while (XAxiDma_Busy(&AxiDma2,XAXIDMA_DMA_TO_DEVICE)) {}
+
+		while (XAxiDma_Busy(&AxiDma2,XAXIDMA_DEVICE_TO_DMA)) {}
+
+		xil_printf("Done sim_time: %d\r\n", i);
+    }
 		/*
 		Status = CheckData();
 		if (Status != XST_SUCCESS) {
